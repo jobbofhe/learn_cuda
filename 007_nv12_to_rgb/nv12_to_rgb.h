b7037ba170912b825cc9188fb7ee61d7ff58ab98
jobbofhe <1254372617@qq.com>
2019-09-30 13:59:13 +0800

    删除冗余代码

--- 007_nv12_to_rgb/nv12_to_rgb.h
+++
@@ -1,8 +0,0 @@
-#ifndef NV12_TO_RGB_H
-#define NV12_TO_RGB_H
-
-typedef unsigned char uchar;
-
-bool nv12_to_rgb(unsigned char *src, unsigned char *dst,int src_width,int src_height, int dst_pitch);
-
-#endif // NV12_TO_RGB_H
